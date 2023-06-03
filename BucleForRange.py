d0ba4d8b9e020c7661b9b67e9ed6f03c0135c757
Carolina Martinez <carolinamtz091@gmail.com>
2023-06-02 23:11:05 -0600

    primera version

---
+++ BucleForRange.py
@@ -0,0 +1,12 @@
+#Bucle for tipo Range
+
+for i in range(10):
+    print("Hola!,soy Caro")
+
+#Para que vaya del 10 al 20
+for i in range(10,21):
+    print(i)
+
+#Para que vaya de dos en dos
+for i in range(20,31,2):#El último dígito indica de cuanto quieres que vaya
+     print(i)
